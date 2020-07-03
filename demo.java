cc060cb7c8129eb6590b3b426136321810bd8b95
atri2151 <57227490+atri2151@users.noreply.github.com>
2020-07-03 15:21:05 +0530

    edit from remote repo

--- demo.java
+++ demo.java
@@ -1,9 +1,10 @@
 public class demo{
 
 	public static void main(String[] args){
-		System.out.print("hinWelcome to git\nfirst change in file\n");
+		System.out.print("hi\nWelcome to git\nfirst change in file\n");
 		System.out.print("new changes can be tracked now\n");
 		System.out.print("we now all set to move forward\n");
+		System.out.print("can be edit from remote repo\n");
 		
 		}
 	}
