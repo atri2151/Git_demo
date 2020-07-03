ea2c02462622ed6217bc4a813f4632188013777e
abhi2151 <atri2151@gmail.com>
2020-07-03 14:50:02 +0530

    it is 4th change

--- demo.java
+++ demo.java
@@ -3,6 +3,7 @@ public class demo{
 	public static void main(String[] args){
 		System.out.print("hinWelcome to git\nfirst change in file\n");
 		System.out.print("new changes can be tracked now\n");
+		System.out.print("we now all set to move forward\n");
 		
 		}
 	}
